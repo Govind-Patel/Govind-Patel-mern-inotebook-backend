bd28c7eafdf155e82f2234d811e4eef2c3ea7f4f
Govind Patel <govind.patel@hostbooks.com>
2024-07-04 19:28:19 +0530

    update note

--- routes/notes.js
+++ routes/notes.js
@@ -39,4 +39,22 @@ router.post('/addnote',fetchuser,[
   }
 })
 
+// Route 3 update an existing  New using: POST "api/notes/updatenote". Login required
+router.put('/updatenote/:id',fetchuser, async (req,res)=>{
+  const {title , description , tag } = req.body;
+  // create a newnote object 
+  const newNote = {};
+  if(title){newNote.title = title};
+  if(description){newNote.description = description};
+  if(tag){newNote.tag = tag};
+  // find the note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if(!note){return res.status(400).send("not found") };
+    if(note.user.toString() !== req.user.id){
+      return res.status(401).send("Not allowed"); 
+    }
+    note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote}, {new:true});
+    res.json({note});
+})
+
 module.exports = router;
