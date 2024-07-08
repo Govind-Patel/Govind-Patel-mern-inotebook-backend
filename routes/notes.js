59357523b8c57a5c0387bb4636b8df5e98e6dae6
Govind Patel <govind.patel@hostbooks.com>
2024-07-08 11:32:45 +0530

    delete endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -39,22 +39,44 @@ router.post('/addnote',fetchuser,[
   }
 })
 
-// Route 3 update an existing  New using: POST "api/notes/updatenote". Login required
+// Route 3 update an existing  New using: PUT "api/notes/updatenote". Login required
 router.put('/updatenote/:id',fetchuser, async (req,res)=>{
   const {title , description , tag } = req.body;
-  // create a newnote object 
-  const newNote = {};
-  if(title){newNote.title = title};
-  if(description){newNote.description = description};
-  if(tag){newNote.tag = tag};
-  // find the note to be updated and update it
+  try {
+    // create a newnote object 
+    const newNote = {};
+    if(title){newNote.title = title};
+    if(description){newNote.description = description};
+    if(tag){newNote.tag = tag};
+    // find the note to be updated and update it
+      let note = await Notes.findById(req.params.id);
+      if(!note){return res.status(400).send("not found") };
+      if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not allowed"); 
+      }
+      note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote}, {new:true});
+      res.json({note});
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+})
+// Route 4 delete an existing  New using: DELETE "api/notes/deletenote". Login required
+router.delete('/deletenote/:id',fetchuser, async (req,res)=>{
+  try {
+    // find the note to be updated and update it
     let note = await Notes.findById(req.params.id);
+
     if(!note){return res.status(400).send("not found") };
+
     if(note.user.toString() !== req.user.id){
       return res.status(401).send("Not allowed"); 
     }
-    note = await Notes.findByIdAndUpdate(req.params.id,{$set:newNote}, {new:true});
-    res.json({note});
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({"success":"Note hsa been deleted",note:note})
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
 })
-
 module.exports = router;
