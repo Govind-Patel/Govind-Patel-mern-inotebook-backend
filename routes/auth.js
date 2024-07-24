78652731b98fe51c2768c75cb404ae4a48de7e77
Govind Patel <govind.patel@hostbooks.com>
2024-07-24 12:22:39 +0530

    response status

--- routes/auth.js
+++ routes/auth.js
@@ -56,6 +56,7 @@ router.post('/login',[
         body("password",'Password cannot be blank').exists(),
     ],  async(req,res)=>{
         const errors = validationResult(req); 
+        let success = false;
         if (!errors.isEmpty()) {
             res.status(400).json({ errors: errors.array() });
         }
@@ -63,11 +64,13 @@ router.post('/login',[
         try {
             let user = await User.findOne({email});
             if(!user){
-                res.status(400).json({errors:"Please try to login with correct credentials"});
+                success = false;
+                res.status(400).json({ errors:"Please try to login with correct credentials"});
             }
             const passCompare = await bcrypt.compare(password,user.password);
             if(!passCompare){
-                res.status(400).json({errors:"Please try to login with correct credentials"});
+                success = false;
+                res.status(400).json({success,errors:"Please try to login with correct credentials"});
             }
             const data = {
                 user:{
@@ -75,7 +78,8 @@ router.post('/login',[
                 }
             }
             const authToken = jwt.sign(data,JWT_SECRET);
-            res.json({authToken});
+            success = true
+            res.json({success,authToken});
         } catch (error) {
             console.log(error.message);
             // res.status(500).send("Internal server error");
